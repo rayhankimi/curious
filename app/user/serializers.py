39ba6b65af2402c9f4008bff768101491f26ea39
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-10-14 20:27:14 +0800

    Fix linting

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model, authenticate
 
-from rest_framework import serializers, authtoken
 from django.utils.translation import gettext_lazy as _
+
+from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -80,7 +80,9 @@ class PublicUserTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-        user_exist = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exist = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
         self.assertFalse(user_exist)
 
     def test_create_token_for_user(self):
