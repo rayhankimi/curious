b14f8fc82669bda7039a9a70b6d66032865df3e1
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-11-11 17:25:09 +0800

    Configure nested routers for urls

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,20 +1,23 @@
-"""
-Url Mapping for user api
-"""
-
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
-from user import views
+from user import views as user_views
 from iotdevice import views as device_views
 
-router = DefaultRouter()
+# Main Router
+router = routers.DefaultRouter()
 router.register('device', device_views.DeviceViewSet)
+
+# Nested Router
+device_router = routers.NestedDefaultRouter(router, 'device', lookup='device')
+device_router.register('value', device_views.DeviceValueViewSet, basename='device-value')
+
 app_name = 'user'
 
 urlpatterns = [
-    path('create', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.CreateTokenView.as_view(), name='token'),
-    path('person/', views.ManageUserView.as_view(), name='person'),
+    path('create', user_views.CreateUserView.as_view(), name='create'),
+    path('token/', user_views.CreateTokenView.as_view(), name='token'),
+    path('person/', user_views.ManageUserView.as_view(), name='person'),
     path('', include(router.urls)),
+    path('', include(device_router.urls)),
 ]
